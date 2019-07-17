a741ab1e44874e6210fb8a7f515672a59004f909
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-07-17 14:02:00 +0700

    List Flutter - Add Icon, Add Set for saved data

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
 class RandomWordsState extends State<RandomWords> { //State Class
 
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18);
 
   @override
@@ -64,10 +65,15 @@ class RandomWordsState extends State<RandomWords> { //State Class
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.green : null,
       ),
     );
   }
