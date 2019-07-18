a9234eddf0208e04838e489348beeabadc35a2b4
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-07-18 13:42:00 +0700

    Pick Images - Add Class to hold View Images

---
+++ lib/view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ViewImages extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+
+}
+
+class AccessState extends State<ViewImages> {
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
