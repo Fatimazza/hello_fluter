acc6cf732fddb96ee1b0c9213f90132c18940a08
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-07-18 12:01:17 +0700

    Camera App - Initial Stateful Widget Layout

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,16 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return new MaterialApp(
+      home: Scaffold(
+        appBar: new AppBar(
+          title: const Text('Multiple Image Picker App'),
+        ),
+        body: new Center(
+          child: new Text('Select Multiple Images'),
+        ),
+      ),
+    );
   }
 }
 
