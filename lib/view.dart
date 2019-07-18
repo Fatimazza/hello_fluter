ee5ba8f1bf6a872759b31b604e50008928830322
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-07-18 13:51:36 +0700

    Pick Images - Add Fields for View Images class

--- lib/view.dart
+++ lib/view.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 
 class ViewImages extends StatefulWidget {
+  final int _index;
+  final Asset _asset;
+
+  ViewImages(
+      this._index, this._asset, {Key key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return null;
-  }
-
+  State<StatefulWidget>
+  createState() => AccessState();
 }
 
 class AccessState extends State<ViewImages> {
-  
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return null;
   }
-
 }
