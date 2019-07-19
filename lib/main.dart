b62a92a68f54dda7655bf3ca02c98b3f0de22f5e
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-07-19 11:12:32 +0700

    Routing App - Add Second Page with Back Button

--- lib/main.dart
+++ lib/main.dart
@@ -48,3 +48,22 @@ class FirstPage extends StatelessWidget {
     );
   }
 }
+
+class SecondPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Second Page'),
+        ),
+        body: Center(
+          child: RaisedButton(child: Text('Back'), onPressed: () {
+            Navigator.pop(context);
+          }),
+        ),
+      ),
+    );
+  }
+}
